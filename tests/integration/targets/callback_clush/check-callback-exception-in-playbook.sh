c321b41bb654a0c497142e6f821586b144c39b7f
Simon Leary <simon.leary42@proton.me>
2025-01-08 14:53:33 -0500

    delete old test files

--- tests/integration/targets/callback_clush/check-callback-exception-in-playbook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-playbook_path="$1"
-tempfile="$(mktemp)" || exit 2
-ansible-playbook "$playbook_path" &>"$tempfile" || exit 2
-grep -qi "failure using method .* in callback plugin" "$tempfile" && exit 1
