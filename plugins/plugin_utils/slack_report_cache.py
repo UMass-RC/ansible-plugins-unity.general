156d47caf05abc53d08bac25d4d4c1f8e897113c
Simon Leary <simon.leary42@proton.me>
2025-01-23 15:41:49 -0500

    md5 only takes bytes

--- plugins/plugin_utils/slack_report_cache.py
+++ plugins/plugin_utils/slack_report_cache.py
@@ -14,7 +14,7 @@ def add_line(line: str, plugin_options: dict, end="\n") -> None:
     plugin_options is the result from AnsiblePlugin.get_options()
     your plugin must extend the unity.general.ramdisk_cache documentation fragment
     """
-    name = f"slack_report_cache.add_line.{md5(line).hexdigest()[:5]}"
+    name = f"slack_report_cache.add_line.{md5(line.encode()).hexdigest()[:5]}"
     with RamdiskCacheContextManager("slack-report", plugin_options, name=name) as cache_file:
         cache_file.seek(0, io.SEEK_END)  # seek to end of file
         cache_file.write(line)
