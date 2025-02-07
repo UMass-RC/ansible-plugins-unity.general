be8bc8fe458c859f3ae8423c6588e3d6ec98d77d
Simon Leary <simon.leary42@proton.me>
2025-02-06 23:05:20 -0500

    revert display_ok_hosts display_skipped_hosts to original behavior

--- plugins/doc_fragments/default_callback_default_options.py
+++ plugins/doc_fragments/default_callback_default_options.py
@@ -8,7 +8,7 @@ class ModuleDocFragment(object):
         pretty_results:
           default: true
         display_ok_hosts:
-          default: true
+          default: false
         display_skipped_hosts:
-          default: true
+          default: false
     """
