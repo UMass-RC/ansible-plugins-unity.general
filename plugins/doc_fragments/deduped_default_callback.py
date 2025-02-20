9da0d012d6925135855eea4cdadbb31d9868f0ea
Simon Leary <simon.leary42@proton.me>
2025-02-19 21:24:46 -0500

    move wrap_text option downwards

--- plugins/doc_fragments/deduped_default_callback.py
+++ plugins/doc_fragments/deduped_default_callback.py
@@ -1,18 +1,6 @@
 class ModuleDocFragment(object):
     DOCUMENTATION = r"""
       options:
-        wrap_text:
-          description: |
-            whether or not to hard-wrap text in callback output.
-            this helps the readability of the left margin of the output, but makes the right margin
-            awkward and makes the output less useful to copy/paste.
-          type: bool
-          default: true
-          ini:
-            - section: callback
-              key: wrap_text
-          env:
-            - name: CALLBACK_WRAP_TEXT
         check_mode_markers:
           description: see default callback documentation
           default: true
@@ -28,4 +16,16 @@ class ModuleDocFragment(object):
         display_skipped_hosts:
           description: see default callback documentation
           default: false
+        wrap_text:
+          description: |
+            whether or not to hard-wrap text in callback output.
+            this helps the readability of the left margin of the output, but makes the right margin
+            awkward and makes the output less useful to copy/paste.
+          type: bool
+          default: true
+          ini:
+            - section: defaults
+              key: wrap_text
+          env:
+            - name: ANSIBLE_WRAP_TEXT
     """
