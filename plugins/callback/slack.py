5a5df68e7668f398cf65c30553783c283aa67da2
Simon Leary <simon.leary42@proton.me>
2025-03-06 11:24:44 -0500

    fix function name again, fix args, remove fstring

--- plugins/callback/slack.py
+++ plugins/callback/slack.py
@@ -57,11 +57,11 @@ class CallbackModule(CallbackBase):
     def get_options(self):
         return self._plugin_options
 
-    def v2_plabook_on_stats(self) -> None:
+    def v2_playbook_on_stats(self, _):
         report_lines = slack_report_cache.get_lines(self.get_options())
         slack_report_cache.flush(self.get_options())
         if not report_lines:
-            display.warning(f"slack: no report lines found!")
+            display.warning("slack: no report lines found!")
             return
         report = "\n".join(report_lines)
         try:
