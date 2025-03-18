eaee68dec8d76228b854ff1116e83f20ad643e90
Simon Leary <simon.leary42@proton.me>
2025-03-18 10:38:18 -0400

    monochrome beartype

--- plugins/plugin_utils/beartype.py
+++ plugins/plugin_utils/beartype.py
@@ -1,6 +1,9 @@
 # if user does not have beartype installed, just don't use it
 try:
-    from beartype import beartype
+    from beartype import beartype as _beartype
+    from beartype import BeartypeConf
+
+    beartype = _beartype(conf=BeartypeConf(is_color=False))
 except ImportError:
 
     def beartype(func):
