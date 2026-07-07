9d802b87b4c980a77d26665800167dad2f30d7bf
Simon Leary <simon.leary42@proton.me>
2026-07-06 18:33:14 -0600

    switch from clinfo to lspci

--- plugins/module_utils/common.py
+++ plugins/module_utils/common.py
@@ -1,4 +1,5 @@
 import re
+import jc
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -16,6 +17,9 @@ def all_elements_equal(x: list) -> bool:
 def _check_output(argv: list[str], _module: AnsibleModule, timeout_sec=0) -> str:
     _, stdout, _ = _module.run_command(["timeout", "-v", str(timeout_sec)] + argv, check_rc=True)
     return stdout
+
+
+NVIDIA_PCI_VENDOR_ID = "10de:"
 
 
 def translate_nvidia_gpu_model_name(model_name: str) -> str:
@@ -45,11 +49,33 @@ def translate_nvidia_gpu_model_name(model_name: str) -> str:
     model_name = model_name.replace("tesla", "")
     model_name = model_name.replace("gtx", "")
     model_name = model_name.replace("hbm3", "")
+    model_name = model_name.replace("rev. a", "")
+    model_name = model_name.replace("a2 / a16", "a16")
+    model_name = model_name.replace("6000/8000", "8000")
     if "8000" not in model_name:
         model_name = model_name.replace("rtx", "")
+    model_name = re.sub(r"\d+gb / \d+gb", "", model_name)
     model_name = re.sub(r"\d+gb", "", model_name)
     model_name = model_name.replace("pcie", "")
     model_name = re.sub(r"sxm\d+", "", model_name)
     model_name = model_name.strip("_- ")
     model_name = re.sub(r"\s+", "_", model_name)
     return model_name
+
+
+def get_gpu_model_and_count(_module) -> tuple[str, int]:
+    # `-q` means query the online PCI ID database, `-mmv` is required by `jc`
+    stdout = _check_output(
+        ["lspci", "-q", "-mmv", "-d", NVIDIA_PCI_VENDOR_ID], _module, timeout_sec=5
+    )
+    # raw=True because `physlot_int` is broken for some nodes (ex: gpu042)
+    nvidia_pci_devices = jc.parse("lspci", stdout, raw=True)
+    nvidia_gpus = [
+        x
+        for x in nvidia_pci_devices
+        if x["class"] in ["3D controller", "VGA compatible controller"]
+    ]
+    # extract from square brackets - example: "GH100 [H200 NVL]"
+    nvidia_gpu_models = [re.sub(r"^.*\[(.*)\].*", r"\1", x["device"]) for x in nvidia_gpus]
+    assert all_elements_equal(nvidia_gpu_models)
+    return translate_nvidia_gpu_model_name(nvidia_gpu_models[0]), len(nvidia_gpu_models)
--- plugins/modules/gpu_model_facts.py
+++ plugins/modules/gpu_model_facts.py
@@ -5,7 +5,7 @@ name: gpu_model_facts
 short_description: facts module that finds the nvidia GPU model name
 description: ""
 requirements:
-  - clinfo
+  - jc
 author: Simon Leary <simon.leary42@proton.me>
 version_added: 2.19
 """
@@ -18,30 +18,17 @@ gpu_model:
   sample: "a100"
 """
 
-import re
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.unity.general.plugins.module_utils.common import (
-    _check_output,
-    all_elements_equal,
-    translate_nvidia_gpu_model_name,
+    get_gpu_model_and_count,
 )
-
-
-def get_gpu_model(_module) -> str:
-    clinfo_out = _check_output(["clinfo"], _module, timeout_sec=5)
-    gpu_lines = [x for x in clinfo_out.splitlines() if x.strip().startswith("Device Name")]
-    if len(gpu_lines) == 0:
-        _module.fail_json("no GPUs found!")
-    gpu_models = [re.sub(r"^\s*Device Name\s+(.*?)\s*$", r"\1", x) for x in gpu_lines]
-    assert all_elements_equal(gpu_models)
-    return translate_nvidia_gpu_model_name(gpu_models[0])
 
 
 def main():
     _module = AnsibleModule(argument_spec={}, supports_check_mode=True)
     _module.exit_json(
         ansible_facts={
-            "gpu_model": get_gpu_model(_module),
+            "gpu_model": get_gpu_model_and_count(_module)[0],
         }
     )
 
