806a7a281654329ba06dc2965bb32fc0597e68aa
Simon Leary <simon.leary42@proton.me>
2026-07-09 20:07:18 +0000

    update shib

--- roles/shibboleth/files/update-filtered-incommon-metadata.bash
+++ roles/shibboleth/files/update-filtered-incommon-metadata.bash
@@ -1,6 +1,29 @@
 set -euo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+function help() {
+    echo "Usage: $0 [--restart]" >&2
+}
+
+restart=0
+while (($# > 0)); do
+    case "$1" in
+        --help)
+            help
+            exit 0
+            ;;
+        --restart)
+            restart=1
+            ;;
+        *)
+            echo "Unknown argument: $1" >&2
+            help
+            exit 2
+            ;;
+    esac
+    shift
+done
+
 tmpfile=/etc/shibboleth/filtered-incommon-metadata.xml.new
 truncate --size 0 "$tmpfile"
 chmod 644 "$tmpfile"
@@ -11,17 +34,18 @@ if cmp -s "$outfile" "$tmpfile"; then
     rm "$tmpfile"
     exit 0
 fi
-mv "$outfile" "$outfile.bak"
-mv "$tmpfile" "$outfile"
+(set -x;  mv "$outfile" "$outfile.bak" && mv "$tmpfile" "$outfile")
 if ! bash /etc/shibboleth/sanity-check.bash; then
-    mv "$outfile" "$outfile.broken"
-    mv "$outfile.bak" "$outfile"
-    echo "old metadata restored."
-    echo "failure-causing metadata moved to '$PWD/filtered-incommon-metadata.xml.broken'."
+    (set -x; mv "$outfile" "$outfile.broken" && mv "$outfile.bak" "$outfile")
+    echo "sanity check failed! old metadata restored, and shibd not restarted."
     exit 1
 fi
 if systemctl status shibd >/dev/null; then
-    systemctl restart shibd
+    if [ "$restart" == 1 ]; then
+        (set -x; systemctl restart shibd)
+    else
+        echo "shibd should be restarted! (you can also call this script with --restart)"
+    fi
 else
     echo "shibd service not running, so not restarted."
 fi
